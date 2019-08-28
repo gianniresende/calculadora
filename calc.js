795b508e9ff6426533dafbf1b124c4064db3e5f0
gianniresende <gianniresende@gmail.com>
2019-08-28 19:47:49 -0300

    add default ao bloco case

--- calc.js
+++ calc.js
@@ -4,64 +4,69 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question('Digite o primeiro número: ', (number1) => {
-  
-    rl.question('Qual operação deseja fazer?: ', (op) => {
-      rl.question('Digite o segundo número: ', (number2) => {
+rl.question('Digite o primeiro número: ', (number1) => {  
+  rl.question('Qual operação deseja fazer?: ', (op) => {
+    rl.question('Digite o segundo número: ', (number2) => {
       let secund_number = number2;
       let first_number = number1;
       switch(op){
-      case '/':        
-        if  (parseFloat(number1) > 5 && parseFloat(number2) < 20){          
-          let result = parseFloat(first_number) / parseFloat(secund_number);
-          console.log(result);
-        }
-        else{
-          console.log('Digite um número entre 5 e 20');
-        }      
-        rl.close();
-        return;
-      case '*': 
-        if  (parseFloat(number1) > 5 && parseFloat(number2) < 20){          
-          let result = parseFloat(first_number) * parseFloat(secund_number);
-          console.log(result);
-        }
-        else{
-          console.log('Digite um número entre 5 e 20');
-        }      
-        rl.close();
-        return;
-      case '+': 
-        if  (parseFloat(number1) > 5 && parseFloat(number2) < 20){          
-          let result = parseFloat(first_number) + parseFloat(secund_number);
-          console.log(result);
-        }
-        else{
-          console.log('Digite um número entre 5 e 20');
-        }      
-        rl.close();
-        return;   
-      case '-':
-        if (parseFloat(number1) > 5 && parseFloat(number2) < 20){          
-          let result = parseFloat(first_number) - parseFloat(secund_number);
-          console.log(result);
-        }
-        else{
-          console.log('Digite um número entre 5 e 20');
-        }      
-        rl.close();
-        return; 
-      case '%':
-        if (parseFloat(number1) > 0 && parseFloat(number2) < 500){          
-          let result = parseFloat(first_number) * parseFloat(secund_number) / 100;
-          console.log(result);
-        }
-        else{
-          console.log('Digite um número entre 5 e 20');
-        }      
-        rl.close();
-        return;   
-    }
-  });
+        case '/':        
+          if  (parseFloat(number1) > 5 && parseFloat(number2) < 20){          
+            let result = parseFloat(first_number) / parseFloat(secund_number);
+            console.log(result);
+          }
+          else{
+            console.log('Digite um número entre 5 e 20');
+          }      
+          rl.close();
+          return;
+        case '*': 
+          if  (parseFloat(number1) > 5 && parseFloat(number2) < 20){          
+            let result = parseFloat(first_number) * parseFloat(secund_number);
+            console.log(result);
+          }
+          else{
+            console.log('Digite um número entre 5 e 20');
+          }      
+          rl.close();
+          return;
+        case '+': 
+          if  (parseFloat(number1) > 5 && parseFloat(number2) < 20){          
+            let result = parseFloat(first_number) + parseFloat(secund_number);
+            console.log(result);
+          }
+          else{
+            console.log('Digite um número entre 5 e 20');
+          }      
+          rl.close();
+          return;   
+        case '-':
+          if (parseFloat(number1) > 5 && parseFloat(number2) < 20){          
+            let result = parseFloat(first_number) - parseFloat(secund_number);
+            console.log(result);
+          }
+          else{
+            console.log('Digite um número entre 5 e 20');
+          }      
+          rl.close();
+          return; 
+        case '%':
+          if (parseFloat(number1) > 0 && parseFloat(number2) < 500){          
+            let result = parseFloat(first_number) * parseFloat(secund_number) / 100;
+            console.log(result);
+          }
+          else{
+            console.log('Digite um número entre 5 e 20');
+          }      
+          rl.close();
+          return;
+        default :
+          {
+            console.log('Preencha os dados corretamente!');
+            rl.close();
+            return;
+          }   
+      }
+    });
   });
 });
